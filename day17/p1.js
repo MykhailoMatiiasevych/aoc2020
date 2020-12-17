a29f6c177c9e5a7bfcbfe4c135dc1fb0ba3444ce
Mykhailo Matiiasevych <mykhailo.matiiasevych@gmail.com>
2020-12-18 00:47:46 +0100

    Day17

---
+++ day17/p1.js
@@ -0,0 +1,65 @@
+const readToLines = require('../common/readToLines')
+const input = readToLines('./input.txt')
+  .map(s => s.trim())
+  .filter(Boolean)
+
+const d = [-1, 0, 1]
+const buildKey = (x, y, z) => `${x}:${y}:${z}`
+const parseKey = key => key.split(':').map(Number)
+const getNeighbors = key => {
+  const [x, y, z] = parseKey(key)
+  return d
+    .reduce(
+      (acc1, dx) =>
+        d.reduce(
+          (acc2, dy) =>
+            d.reduce(
+              (acc3, dz) => [...acc3, buildKey(x + dx, y + dy, z + dz)],
+              acc2
+            ),
+          acc1
+        ),
+      []
+    )
+    .filter(n => n !== key)
+}
+
+const activeNeighborsCount = (state, key) =>
+  getNeighbors(key).reduce((sum, nKey) => (state.has(nKey) ? sum + 1 : sum), 0)
+
+const doSteps = (state, remainSteps) => {
+  if (remainSteps === 0) return state
+  const newState = new Set()
+  const inactive = new Set()
+  state.forEach(key => {
+    getNeighbors(key)
+      .filter(key => !state.has(key))
+      .forEach(key => inactive.add(key))
+  })
+
+  state.forEach(key => {
+    const count = activeNeighborsCount(state, key)
+    if (count >= 2 && count <= 3) newState.add(key)
+  })
+  inactive.forEach(key => {
+    const count = activeNeighborsCount(state, key)
+    if (count === 3) newState.add(key)
+  })
+  return doSteps(newState, remainSteps - 1)
+}
+
+// Key: x:y:z
+const state = input.reduce(
+  (a1, s, y) =>
+    s
+      .split('')
+      .reduce(
+        (acc, c, x) => (c === '#' ? acc.add(buildKey(x, y, 0)) : acc),
+        a1
+      ),
+  new Set()
+)
+
+const totalSteps = 6
+
+console.log(doSteps(state, totalSteps).size)
